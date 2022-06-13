c5e2e90941c374bc210fb9ebeafca8687ba787f6
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-13 22:07:09 +0100

    Added functionality for the extended nav to the sideNav

--- components/nav/SideNavItem.tsx
+++ components/nav/SideNavItem.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import {Dispatch, SetStateAction} from 'react';
 
 interface SideNavItemProps {
@@ -5,14 +6,17 @@ interface SideNavItemProps {
     iconPath: string;
     isActive: boolean;
     setActiveNav?: Dispatch<SetStateAction<string>>;
+    isExtended?: boolean;
 }
 
-const SideNavItem : React.FC<SideNavItemProps> = ({name, iconPath, isActive, setActiveNav}) => {
+const SideNavItem : React.FC<SideNavItemProps> = ({name, iconPath, isActive, setActiveNav, isExtended}) => {
     return (
-        <div onClick={setActiveNav ? () => setActiveNav(name) : null} className={`transition-all flex gap-x-[22px] text-white items-center ${isActive ? "" : "opacity-50"} cursor-pointer`}>
-            <img src={iconPath} />
-            <span style={{opacity: isActive ? 100 : 50}}>{name}</span>
-        </div>
+        <Link href={name == "My Projects" ? "projects" : name.toLocaleLowerCase()}>
+            <div onClick={setActiveNav ? () => setActiveNav(name) : null} className={`transition-all flex gap-x-[22px] text-white items-center ${isActive ? "" : "opacity-50"} cursor-pointer`}>
+                <img src={iconPath} />
+                <span style={{opacity: isExtended ? (isActive ? 100 : 50) : 0}}>{name}</span>
+            </div>
+        </Link>
     )
 }
 export default SideNavItem;

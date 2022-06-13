ab6aee93a3c048382e0d226184e61b439ef0eaf8
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-13 22:26:26 +0100

    Created the scrapbook page

---
+++ pages/scrapbook/index.tsx
@@ -0,0 +1,13 @@
+import SideNav from "../../components/nav/SideNav";
+import TopNav from "../../components/nav/TopNav";
+import { useState } from 'react';
+import { useRouter } from "next/router";
+import NavWrapper from "../../components/nav/NavWrapper";
+
+export default function scrapbook () {
+    return (
+        <NavWrapper>
+            THe scrapbook
+        </NavWrapper>
+    );
+}

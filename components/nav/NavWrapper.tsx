2720ad18739d4f0467049dac0532b0b10c00df8d
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-13 22:30:12 +0100

    Finished working on the navbars

--- components/nav/NavWrapper.tsx
+++ components/nav/NavWrapper.tsx
@@ -5,13 +5,13 @@ import { useRouter } from "next/router";
 
 const NavWrapper = ({children}) => {
     const [activeSideNav, setActiveSideNav] = useState("Home");
-    const [activeTopNav, setActiveTopNav] = useState("");
+    const [setActiveTopNav] = useState("");
     const router = useRouter();
     const [extended, setExtended] = useState(router.pathname !== "/messages");
     return (
     <>
         <SideNav activeNav={activeSideNav} setActiveNav={setActiveSideNav} extended={extended}/>
-        <TopNav activeTopNav={activeTopNav} setActiveTopNav={setActiveTopNav} extendedNav={extended}/>
+        <TopNav setActiveTopNav={setActiveTopNav} extendedNav={extended}/>
         <div style={{marginLeft : extended ? 178 : 64, width: extended ? 'calc(100vw - 178px)' : 'calc(100vw - 74px)'}} className="transition-all pt-[120px] pl-[30px] pr-[45px] xl:pl-[45px] xl:pr-[60px]">
             {children}
         </div>
--- components/nav/TopNav.tsx
+++ components/nav/TopNav.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import NavSearch from "./NavSearch";
 import TopNavIcon from "./TopNavIcon";
 import TopNavItem from "./TopNavItem";
 
-const TopNav = ({extendedNav, activeTopNav, setActiveTopNav}) => {
+const TopNav = ({extendedNav, setActiveTopNav}) => {
+    const router = useRouter();
+    console.log(router.pathname)
     return (
         <nav style={{width: extendedNav ? 'calc(100vw - 178px)' : 'calc(100vw - 74px)'}} className="transition-all fixed right-0 w-[calc(100vw-178px)] h-[120px] shadow-[0_2px_10px_0_rgba(0,0,0,0.3)] pl-[30px] pr-[45px] xl:pl-[45px] xl:pr-[60px]">
             <div className="flex h-full max-w-[1230px] items-center">
                 <section className="flex flex-row gap-[20px] xl:gap-[40px] mr-[78px]">
-                    <TopNavItem name="SCRAPBOOK" path="" isActive={activeTopNav == "scrapbook"} setActiveTopNav={setActiveTopNav}/>
-                    <TopNavItem name="PROJECTS" path="" isActive={activeTopNav == "projects"} setActiveTopNav={setActiveTopNav}/>
-                    <TopNavItem name="PEOPLE" path="" isActive={activeTopNav == "people"} setActiveTopNav={setActiveTopNav}/>
-                    <TopNavItem name="JOBS" path="" isActive={activeTopNav == "jobs"} setActiveTopNav={setActiveTopNav}/> 
+                    <TopNavItem name="SCRAPBOOK" path="" isActive={router.pathname == "/scrapbook"} setActiveTopNav={setActiveTopNav}/>
+                    <TopNavItem name="PROJECTS" path="" isActive={router.pathname == "/projects"} setActiveTopNav={setActiveTopNav}/>
+                    <TopNavItem name="PEOPLE" path="" isActive={router.pathname == "/people"} setActiveTopNav={setActiveTopNav}/>
+                    <TopNavItem name="JOBS" path="" isActive={router.pathname == "/jobs"} setActiveTopNav={setActiveTopNav}/> 
                 </section>
                 <div className="flex flex-row gap-x-[24px] w-full max-w-[950px] justify-end">
                     <NavSearch />

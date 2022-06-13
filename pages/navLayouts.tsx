368f47b04b16ceaa9c6dde46368588205e87688d
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-13 14:15:28 +0100

    Added the topNav to Nav Layout

--- pages/navLayouts.tsx
+++ pages/navLayouts.tsx
@@ -1,8 +1,10 @@
 import SideNav from "../components/nav/SideNav";
+import TopNav from "../components/nav/TopNav";
 
 
 export default function NavLayouts () {
     return (<div>
         <SideNav />
+        <TopNav />
     </div>)
 }

d15f9d302bc338720e336e47d2c563dfc3ae9a3c
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-13 22:26:37 +0100

    Created the NavWrapper component

---
+++ components/nav/NavWrapper.tsx
@@ -0,0 +1,22 @@
+import SideNav from "./SideNav";
+import TopNav from "./TopNav";
+import { useState } from 'react';
+import { useRouter } from "next/router";
+
+const NavWrapper = ({children}) => {
+    const [activeSideNav, setActiveSideNav] = useState("Home");
+    const [activeTopNav, setActiveTopNav] = useState("");
+    const router = useRouter();
+    const [extended, setExtended] = useState(router.pathname !== "/messages");
+    return (
+    <>
+        <SideNav activeNav={activeSideNav} setActiveNav={setActiveSideNav} extended={extended}/>
+        <TopNav activeTopNav={activeTopNav} setActiveTopNav={setActiveTopNav} extendedNav={extended}/>
+        <div style={{marginLeft : extended ? 178 : 64, width: extended ? 'calc(100vw - 178px)' : 'calc(100vw - 74px)'}} className="transition-all pt-[120px] pl-[30px] pr-[45px] xl:pl-[45px] xl:pr-[60px]">
+            {children}
+        </div>
+    </>
+    );
+
+}
+export default NavWrapper;

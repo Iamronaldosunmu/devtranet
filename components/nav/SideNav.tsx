c5024ba764cd0c630194246d0de962a3b5989fb0
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-13 14:19:13 +0100

    Added the extended prop to all the nav items

--- components/nav/SideNav.tsx
+++ components/nav/SideNav.tsx
@@ -4,19 +4,20 @@ import SideNavItem from "./SideNavItem";
 
 const SideNav = (props) => {
     const [activeNav, setActiveNav] = useState("Home");
+    const [extended, setExtended] = useState(true);
     return (
         <nav className="fixed left-0 pt-[35px] w-[178px] bg-[#5F6FEE] h-screen ">
             <div className="flex justify-center mb-[60px]">
                 <img src="./assets/dynamicsLogo.svg" />
             </div>
             <section className="flex flex-col gap-y-[40px] pl-[27px] mb-[90px]">
-                <SideNavItem setActiveNav={setActiveNav} name="Home" iconPath="./assets/homeIcon.svg" isActive={activeNav == "Home"} />
-                <SideNavItem setActiveNav={setActiveNav} name="Community" iconPath="./assets/communityIcon.svg" isActive={activeNav == "Community"} />
-                <SideNavItem setActiveNav={setActiveNav} name="Workshops" iconPath="./assets/workshopsIcon.svg" isActive={activeNav == "Workshops"} />
-                <SideNavItem setActiveNav={setActiveNav} name="My Projects" iconPath="./assets/myProjectsIcon.svg" isActive={activeNav == "My Projects"} />
-                <SideNavItem setActiveNav={setActiveNav} name="Events" iconPath="./assets/eventsIcon.svg" isActive={activeNav == "Events"} />
-                <SideNavItem setActiveNav={setActiveNav} name="Blog" iconPath="./assets/blogIcon.svg" isActive={activeNav == "Blog"} />
-                <SideNavItem setActiveNav={setActiveNav} name="Settings" iconPath="./assets/settingsIcon.svg" isActive={activeNav == "Settings"} />
+                <SideNavItem isExtended={extended} setActiveNav={setActiveNav} name="Home" iconPath="./assets/homeIcon.svg" isActive={activeNav == "Home"} />
+                <SideNavItem isExtended={extended} setActiveNav={setActiveNav} name="Community" iconPath="./assets/communityIcon.svg" isActive={activeNav == "Community"} />
+                <SideNavItem isExtended={extended} setActiveNav={setActiveNav} name="Workshops" iconPath="./assets/workshopsIcon.svg" isActive={activeNav == "Workshops"} />
+                <SideNavItem isExtended={extended} setActiveNav={setActiveNav} name="My Projects" iconPath="./assets/myProjectsIcon.svg" isActive={activeNav == "My Projects"} />
+                <SideNavItem isExtended={extended} setActiveNav={setActiveNav} name="Events" iconPath="./assets/eventsIcon.svg" isActive={activeNav == "Events"} />
+                <SideNavItem isExtended={extended} setActiveNav={setActiveNav} name="Blog" iconPath="./assets/blogIcon.svg" isActive={activeNav == "Blog"} />
+                <SideNavItem isExtended={extended} setActiveNav={setActiveNav} name="Settings" iconPath="./assets/settingsIcon.svg" isActive={activeNav == "Settings"} />
             </section>
             <div className="pl-[27px]">
                 <SideNavItem name="Logout" iconPath="./assets/logoutIcon.svg" isActive={true}/>
